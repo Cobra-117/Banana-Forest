2538bc6045ddb9596c0cef4b4739488eaaf32f5c
Timothé Lacheroy <tLacheroy@localhost.localdomain>
2021-12-24 14:29:55 +0100

    add map storage in int array for pathfinding

--- Assets/Scripts/Enemy.cs
+++ Assets/Scripts/Enemy.cs
@@ -7,17 +7,23 @@ public class Enemy : MonoBehaviour
 {
     public Tilemap tilemap;
     private Vector3 pos;
-    private int distanceMap;
+    private int[,] distanceMap;
 
     // Start is called before the first frame update
     void Start()
     {
+        distanceMap = new int[tilemap.size.y, tilemap.size.x];
+        for (int j = 0; j < tilemap.size.y; j++) {
+            for (int i = 0; i < tilemap.size.x; i++) {
+                distanceMap[j, i] = 99;
+            }
+       }   
     }
 
     // Update is called once per frame
     void Update()
     {
         Vector3 cellpos = tilemap.WorldToCell(transform.position);
-        Debug.Log("x: " + cellpos.x + "y: " + cellpos.y);   
+        //Debug.Log("x: " + cellpos.x + "y: " + cellpos.y);   
     }
 }
