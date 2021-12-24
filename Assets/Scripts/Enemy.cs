dfebdd019e5ee07b9e1cbc874bce8517fa6b9acf
Timothé Lacheroy <tLacheroy@localhost.localdomain>
2021-12-24 13:43:45 +0100

    add ennemy place holder

--- Assets/Scripts/Enemy.cs
+++ Assets/Scripts/Enemy.cs
@@ -1,18 +1,23 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Tilemaps;
 
 public class Enemy : MonoBehaviour
 {
+    public Tilemap tilemap;
+    private Vector3 pos;
+    private int distanceMap;
+
     // Start is called before the first frame update
     void Start()
     {
-        
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        Vector3 cellpos = tilemap.WorldToCell(transform.position);
+        Debug.Log("x: " + cellpos.x + "y: " + cellpos.y);   
     }
 }
