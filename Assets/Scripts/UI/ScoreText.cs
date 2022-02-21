d244dc17a7a5b25947cf127a8a74c4cd6babaa09
Cobra-117 <timothe.lacheroy@epitech.eu>
2022-02-21 18:36:42 +0100

    [UI] fix score text

--- Assets/Scripts/UI/ScoreText.cs
+++ Assets/Scripts/UI/ScoreText.cs
@@ -4,15 +4,19 @@ using UnityEngine;
 
 public class ScoreText : MonoBehaviour
 {
+    private GLOBAL global;
+    private TMPro.TextMeshProUGUI txt;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        global = GameObject.FindGameObjectWithTag("Global").GetComponent<GLOBAL>();
+        txt = this.gameObject.GetComponent<TMPro.TextMeshProUGUI>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        txt.text = "Score: " + global.score.ToString();
     }
 }
