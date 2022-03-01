9e03552b5bde6fe677714c45df221afe70db4614
Cobra-117 <timothe.lacheroy@epitech.eu>
2022-03-01 19:27:30 +0100

    [UI] add visible cooldown for rain

--- Assets/Scripts/UI/RainButton.cs
+++ Assets/Scripts/UI/RainButton.cs
@@ -6,10 +6,24 @@ using UnityEngine.EventSystems;
 public class RainButton : MonoBehaviour, IPointerDownHandler
 {
     public acidRain _acidRain;
+    public float cooldown = 3;
+    public GameObject mask;
+
+    void Update()
+    {
+        cooldown -= Time.deltaTime;
+        if (cooldown < 0)
+            cooldown = 0;
+        mask.transform.localScale = new Vector3(1f * (cooldown / 15), mask.transform.localScale.y, mask.transform.localScale.z);
+        mask.transform.localPosition  = new Vector3((63.5f * mask.transform.localScale.x) - 63.5f, mask.transform.localPosition.y, mask.transform.localPosition.x);
+    }
 
     public void OnPointerDown(PointerEventData eventData)
     {
+        if (cooldown > 0)
+            return;
         Debug.Log("clicked");
         _acidRain.ActivateRain();
+        cooldown = 15;
     }
 }
