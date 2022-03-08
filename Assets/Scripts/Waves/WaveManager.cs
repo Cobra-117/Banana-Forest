58cf4600e7e0e56b221741398893ca06b7bf73b8
Cobra-117 <timothe.lacheroy@epitech.eu>
2022-03-08 18:59:10 +0100

    [Wave Manager] set ennemies spawn frequency based on difficulty

--- Assets/Scripts/Waves/WaveManager.cs
+++ Assets/Scripts/Waves/WaveManager.cs
@@ -5,22 +5,40 @@ using UnityEngine.Tilemaps;
 
 public class WaveManager : MonoBehaviour
 {
+    [Header("Settings")]
     public int difficulty;
+    public float SpawnCooldown;
+
+    [Header("Unity")]
     public GameObject[] EnnemiesPrefab;
     public DistanceMap distanceMap;
     public Vector3 SpawnPosition;
+
+    private float countdown = 0;
 
     // Start is called before the first frame update
     void Start()
     {
         SpawnPosition = distanceMap.tilemap.CellToWorld(distanceMap.distanceMapToTileMap(distanceMap.SpawnPosition));
-        GameObject Enemy = Instantiate(EnnemiesPrefab[0]);
-        Enemy.transform.position = SpawnPosition;
+        SpawnPosition = new Vector3(SpawnPosition.x - 0.5f, SpawnPosition.y + 0.32f, SpawnPosition.z);
+        Debug.Log("spawn position :" + SpawnPosition.ToString());
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        countdown -= Time.deltaTime;
+        if (countdown <= 0)
+        {
+            countdown = SpawnCooldown;
+            if (Random.Range(1, 10 - difficulty) == 1)
+                SpawnEnemy();
+        }
+    }
+
+    void SpawnEnemy()
+    {
+        GameObject Enemy = Instantiate(EnnemiesPrefab[0]);
+        Enemy.transform.position = SpawnPosition;
     }
 }
