c9d825f9d9f4f7128b2adaf936ef8cea316f2c82
Cobra-117 <timothe.lacheroy@epitech.eu>
2022-03-13 13:19:32 +0100

    [UI] made Game Over menu buttons functionnal

--- Assets/Scripts/UI/GoToScene.cs
+++ Assets/Scripts/UI/GoToScene.cs
@@ -7,9 +7,16 @@ using UnityEngine.SceneManagement;
 public class GoToScene : MonoBehaviour
 {
     public int SceneIndex;
+    public string sceneName;
 
     public void GoScene()
     {
         SceneManager.LoadScene(SceneIndex);
     }
+
+    public void GoSceneByName()
+    {
+        SceneManager.LoadScene(sceneName);
+    }
+    
 }
