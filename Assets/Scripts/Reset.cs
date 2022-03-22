0f78f24a05e33209d0b16b02196eb2924da17bac
Cobra-117 <timothe.lacheroy@epitech.eu>
2022-03-22 11:59:06 +0100

    [Cleanup] remove unused code

--- Assets/Scripts/Reset.cs
+++ Assets/Scripts/Reset.cs
@@ -5,16 +5,10 @@ using UnityEngine;
 public class Reset : MonoBehaviour
 {
     public int level;
-    public AudioSource audio;
 
     private void Awake()
     {
         GLOBAL.score = 0;
         GLOBAL.currentLevel = level;
-    }
-
-    private void Start()
-    {
-        //audio.PlayOneShot((AudioClip)Resources.Load("Sounds/ForwardBird"));
     }
 }
